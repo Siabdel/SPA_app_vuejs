13d1a3a3e0b4597fff4b10c6675ec3ce533d9415
Django_hp820 <siabdel58@gmail.com>
2025-05-27 12:17:06 +0200

    delete rep SPA

--- SPA/Mawaqit/envMawaqit/src (copie)/docker.sh
+++
@@ -1,1 +0,0 @@
-docker build -t mawaqit-app .
--- SPA/Mawaqit/envMawaqit/src (copie)/main.js
+++
@@ -1,8 +0,0 @@
-// src/main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// Si vous avez un fichier CSS global séparé (autre que celui dans App.vue)
-// import './assets/main.css'
-
-createApp(App).mount('#app')
